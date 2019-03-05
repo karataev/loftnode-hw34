ad50bd6e7a89d6d48a510738ad4edbfa55166689
karataev <eugene.karataev@gmail.com>
2019-03-05 21:58:07 +0700

    Error handling

--- server/index.js
+++ server/index.js
@@ -16,6 +16,20 @@ storage.init();
 
 const app = new Koa();
 app.keys = ['secret here'];
+
+app.use( async (ctx, next) => {
+  try {
+    await next();
+    const status = ctx.status || 404;
+    if (status === 404) {
+      ctx.throw(404);
+    }
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.render('error', {error: err});
+  }
+});
+
 app.use(koaBody());
 app.use(serve('./public') );
 
@@ -31,25 +45,6 @@ app.use(router.routes());
 let upload = path.join('./public', 'upload');
 if (!fs.existsSync(upload)) fs.mkdirSync(upload);
 
-app.use( async (ctx, next) => {
-  ctx.body = 'Hello world!';
-});
-
-
 app.listen(PORT, function() {
   console.log(`Server running on http://localhost:${PORT}`)
 });
-
-/*
-app.use((req, res, next) => {
-  let err = new Error('not found');
-  err.status = '404';
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {error: err});
-});
-*/
